2e137e15ab30fdfeb3ab848d5da3be781a26908f
Daniel <daniel.leon@o365.unab.edu.co>
2021-10-12 14:32:33 -0500

    Actualización

--- restaurante-vue/src/views/Menu.vue
+++ restaurante-vue/src/views/Menu.vue
@@ -1,6 +1,5 @@
 
    <template>
-   
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -14,7 +13,7 @@
     <title>MENU</title>
 
     <!--class=" bg-gray-800"
-
+      
     <div><img class=" object-left-top gap-4 w-60 h-24 " src="https://crustier-twists.000webhostapp.com/imagenes/propuesta.png"
     alt="Logo_restaurante" /></div>    
      -->
@@ -23,25 +22,18 @@
   <body
     class="bg-fixed bg-cover bg-center bg-no-repeat h-768 w-full lg:p-8"
     style="
-      background-image: url(https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/restaurante/menu-art.jpg)
+      background-image: url(https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/restaurante/menu-art.jpg);
     "
-    >
-
-
-
-  <body class=" bg-fixed bg-cover bg-center bg-no-repeat h-768 w-full lg:p-8"
-    style="background-image: url(https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/restaurante/menu-art.jpg)">
- 
-
+  >
     <main>
       <div>
         <h2 valor="Hello word">Productos</h2>
         <form action="#" id="form_menu">
-          <label for="producto"> ADICIONALES: </label>
+          <label for="producto"> ENTRADAS: </label>
           <select name="producto" id="producto" v-model="seleccion">
             <option value="-1">Seleccione un producto</option>
 
-            <option  @click.prevent="platosMenu" 
+            <option
               v-for="(unProducto, llave) in listaProductos"
               v-bind:key="llave"
               v-bind:value="llave"
@@ -50,45 +42,13 @@
             </option>
           </select>
 
-          <button
-            id="boton"
-            class="
-              font-semibold py-2 px-4
-              border border-gray-400 rounded shadow
-              bg-gradient-to-r
-              from-green-400
-              to-blue-500
-              hover:from-gray-400
-              hover:to-gray-800
-            "
-            type="button"
-          >
-            AÑADIR +
+          <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+            Añadir
           </button>
           <!-- <select name="" id=""></select> -->
         </form>
       </div>
-
-      <!-- <div>
-        <h2>Listado de seleccion platos</h2>
-        <table border="1">
-          <thead>
-            <tr><th>PRODUCTO</th></tr>
-          </thead>
-           <tbody id="datos">
-            <tr v-for="(unProducto, j) in listaProductos":key="j">
-                <td>
-                  {{unProducto.producto.nombre}} - ${{unProducto.producto.precio}}
-                  </td>
-            </tr> 
-
-          </tbody>
-
-        </table>
-      </div> -->
-      
     </main>
-
 
     <select v-model="selected">
       <option disabled value="">Seleccione un elemento</option>
@@ -99,35 +59,35 @@
     <span>Seleccionado: {{ selected }}</span>
     <p class="text-3xl ...">{{ selected }}</p>
 
-    <select v-model="selected">
-  <option disabled value="">Seleccione un elemento</option>
-  <option>A</option>
-  <option>B</option>
-  <option>C</option>
-  </select>
-  <span>Seleccionado: {{ selected }}</span>
-  <p class="text-3xl ...">{{ selected }}</p>
-
-
+  
     <div
-    v-for="(unProducto, llave) in listaProductos"
-    v-bind:key="llave"
-    v-bind:value="llave"
-  >
-    <img :src="unProducto.link" style="width: 20%" alt="" />
-    <p class="text-3xl  text-gray-900 ...">{{ unProducto.nombre }}</p>
-    <p class="text-3xl ...">{{ unProducto.precio }}</p>
-  </div>
-    </body>
-
-
-
+      
+      class="grid grid-cols-4 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"> 
+      <a 
+      v-for="(unProducto, llave) in listaProductos"
+      v-bind:key="llave"
+      v-bind:value="llave"
+      href="#" class="group">
+        <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+          <img :src="unProducto.link" alt="" class="w-full h-full object-center object-cover group-hover:opacity-75">
+        </div>
+        <h3 class="mt-4 text-sm text-gray-700">
+          {{ unProducto.nombre }}
+        </h3>
+        <p class="mt-1 text-lg font-medium text-gray-900">
+          ${{ unProducto.precio }}
+        </p>
+      </a>      
+    </div>
+  
+  
   </body>
+
 <p></p>
   
 </template>
 
-<script class="mt-1 text-lg font-medium text-gray-900" >
+<script>
 import NavBar from "@/components/NavBar.vue";
 export default {
   components: {
@@ -143,25 +103,25 @@ export default {
     let producto2 = { 
       nombre: "Arepas(De maíz pelao)", 
       precio: 8000,
-      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/entrada%202%20(1).png",
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/Papas%20fritas.jpg",
     };
 
     let producto3 = {
       nombre: "Hormigas Culonas(Platillo exótico y exquisito)",
       precio: 12000,
-      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/c.jpg",
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/entrada.PNG",
     };
 
     let producto4 = { 
-      nombre: "Papas a la Francesa(Bien crujientes)", 
+      nombre: "Papas Fritas(Bien crujientes)", 
       precio: 7000,
       link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/Papas%20fritas.jpg",
       };
 
     let producto5 = {
-      nombre: "Patacones(De los mejores plátanos de nuestra tierra)",
+      nombre: "Patacones(De los mejores plátanos)",
       precio: 7000,
-      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/Captura%203.PNG",
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/Patacon%20con%20Hogao.jpg",
     };
 
     this.listaProductos = [
@@ -179,16 +139,7 @@ export default {
         producto: {},
       },
       seleccion: -1,
-      listaSeleccion: [],
     };
-  },
-
-  methods: {
-    // platosMenu(){
-    //   this.compra.producto = this.listaProductos[this.seleccion];
-    //   this.listaSeleccion.push(this.compra);
-
-    // },
   },
 };
 </script>
@@ -200,24 +151,6 @@ header h1 {
 #boton {
   margin: 1rem;
 }
-
-    header h1{
-        font-size: 6rem;
-    }
-
-    .icono{
-        display: inline-block;
-        margin: 1rem;
-        padding: 0rem; 
-        row-gap: 2px;
-        width: 1.875rem;
-        height: 0.75rem;
-    }
-    #boton{
-        
-        margin: 1rem;
-    }
-  
 
 /* #form_menu{
     display:grid;
