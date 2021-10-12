f57b7c35aa61ebb980100845696f7eab3123a89b
Daniel <daniel.leon@o365.unab.edu.co>
2021-10-12 14:17:21 -0500

    Se agrega id, tipo a entradas-platos fuertes-adicionales

--- restaurante-vue/src/views/Menu.vue
+++ restaurante-vue/src/views/Menu.vue
@@ -29,7 +29,7 @@
       <div>
         <h2 valor="Hello word">Productos</h2>
         <form action="#" id="form_menu">
-          <label for="producto"> ENTRADAS: </label>
+          <label for="producto"> PRODUCTOS: </label>
           <select name="producto" id="producto" v-model="seleccion">
             <option value="-1">Seleccione un producto</option>
 
@@ -38,7 +38,7 @@
               v-bind:key="llave"
               v-bind:value="llave"
             >
-              {{ unProducto.nombre }} - ${{ unProducto.precio }}
+              {{ unProducto.nombre }} - ${{ unProducto.precio }} - {{unProducto.tipo}}
             </option>
           </select>
 
@@ -75,8 +75,13 @@
           {{ unProducto.nombre }}
         </h3>
         <p class="mt-1 text-lg font-medium text-gray-900">
+          {{ unProducto.tipo }}
+        </p>
+
+        <p class="mt-1 text-lg font-medium text-gray-900">
           ${{ unProducto.precio }}
         </p>
+        
         <button class="bg-gradient-to-r from-green-300 to-blue-400 hover:from-gray-400 hover:to-gray-800 text-gray-800 font-semibold lg:m-4 py-2 px-4 border border-gray-600 rounded shadow">
             AÑADIR +
           </button>
@@ -97,35 +102,161 @@ export default {
     NavBar,
   },
   mounted() {
+    //ENTRADAS
     let producto1 = {
+      id: 1,
       nombre: "Pandeyucas(Tradicionales y crocanticos)",
-      precio: 6000,
+      precio: 0,
       link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/pandeyucas.jpg",
-      
+      tipo:"(Entrada)",
     };
 
     let producto2 = { 
+      id:2,
       nombre: "Arepas(De maíz pelao)", 
-      precio: 8000,
+      precio: 0,
       link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/entrada%202%20(1).png",
+      tipo: "(Entrada)",
     };
-
-    let producto3 = {
-      nombre: "Hormigas Culonas(Platillo  exquisito)",
-      precio: 12000,
-      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/c.jpg",
+    //PLATOS FUERTES
+    let producto3= {
+      id: 3,
+      nombre: "Cabro Con Pepitoria(Yuca, Arepa y Ensalada)",
+      precio: 26000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/cabro-1-400x250.jpg",
+      tipo:"(Plato Fuerte)",
     };
 
     let producto4 = { 
+      id:4,
+      nombre: "Filete de Pescado(Arroz Blanco, Patacón y Ensalada)", 
+      precio: 30000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/pescado(act).jpg",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto5 = { 
+      id:5,
+      nombre: "Sancocho de gallina(Arroz,aguacate y yuca)", 
+      precio: 17000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/sancocho-de-gallina(ACT).jpg",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto6 = { 
+      id:6,
+      nombre: "Pollo asado(Yuca, Arepa, Papa y un delicioso ají)", 
+      precio: 22000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/pollo.jpg",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto7 = { 
+      id:7,
+      nombre: "Sobrebarriga(Al Horno: Yuca, Arepa y Ensalada)", 
+      precio: 24000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/sobrebarriga%20act.jpg",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto8 = { 
+      id:8,
+      nombre: "Pechuga a la plancha(Papa a la Francesa y Ensalada)", 
+      precio: 17000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/pechuga%20act.jpg",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto9= {
+      id: 9,
+      nombre: "Cazuela de Mariscos(Arroz Blanco y Patacón)",
+      precio: 34000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/cazuela-act.jpg",
+      tipo:"(Plato Fuerte)",
+    };
+
+    let producto10 = { 
+      id:10,
+      nombre: "Oreada(Arroz Blanco , Yuca, ensalada)", 
+      precio: 19000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/oreada%20act.png",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto11 = { 
+      id:11,
+      nombre: "Pavo relleno(Pure de papa y ensalada agridulce)", 
+      precio: 34000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/pavo%20relleno%20act.jpg",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto12 = { 
+      id:12,
+      nombre: "Arroz Paisa(patacones y aguacate)", 
+      precio: 21000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/arroz%20paisa.jpg",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto13 = { 
+      id:13,
+      nombre: "Ajiaco Santareño(arroz blanco y aguacate)", 
+      precio: 24000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/ajiaco.jpg",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto14 = { 
+      id:14,
+      nombre: "Bandeja Paisa", 
+      precio: 17000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/Bandeja%20act.png",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto15 = { 
+      id:15,
+      nombre: "Mute(Yuca, Arroz y aguacate)", 
+      precio: 14000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/mute%20act.png",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto16 = { 
+      id:16,
+      nombre: "Arroz Con Pollo(papas a la Francesa y aguacate", 
+      precio: 14000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/comidas/arroz-pollo.jpg",
+      tipo: "(Plato Fuerte)",
+    };
+    let producto17 = {
+      id: 17,
+      nombre: "Pandeyucas(Tradicionales y crocanticos)",
+      precio: 6000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/pandeyucas.jpg",
+      tipo:"(Adicionales)",
+    };
+
+    let producto18 = { 
+      id:18,
+      nombre: "Arepas(De maíz pelao)", 
+      precio: 8000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/entrada%202%20(1).png",
+      tipo: "(Adicionales)",
+    };
+
+    let producto19 = {
+      id:19,
+      nombre: "Hormigas Culonas(Platillo  exquisito (30gr))",
+      precio: 12000,
+      link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/c.jpg",
+      tipo: "(Adicionales)",
+    };
+
+    let producto20 = { 
+      id:20,
       nombre: "Papas Fritas(Bien crujientes)", 
       precio: 7000,
       link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/Papas%20fritas.jpg",
+       tipo: "(Adicionales)",
       };
 
-    let producto5 = {
+    let producto21 = {
+      id:21,
       nombre: "Patacones(De los mejores plátanos)",
       precio: 7000,
       link: "https://el-sabor-de-nuestra-tierra.000webhostapp.com/imagenes/Adicionales/Captura%203.PNG",
+       tipo: "(Adicionales)",
     };
 
     this.listaProductos = [
@@ -134,6 +265,24 @@ export default {
       producto3,
       producto4,
       producto5,
+      producto6,
+      producto7,
+      producto8,
+      producto9,
+      producto10,
+      producto11,
+      producto12,
+      producto13,
+      producto14,
+      producto15,
+      producto16,
+      producto17,
+      producto18,
+      producto19,
+      producto20,
+      producto21,
+      
+
     ]; //cuidado, el this
   },
   data() {
